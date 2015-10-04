00c0cd89c5cedd7d99f37da1863dd6ad5c0e3438
Linus Liang <linus.h.liang@gmail.com>
2015-10-04 00:46:53 -0400

    switched to persona choice xibs

---
+++ Curate/PersonaScreen5sand6.swift
@@ -0,0 +1,25 @@
+//
+//  PersonaScreen5sand6.swift
+//  Curate
+//
+//  Created by Linus Liang on 10/3/15.
+//  Copyright © 2015 Kenneth Kuo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PersonaScreen5sand6: UIView {
+    @IBOutlet weak var techPersona: UIImageView!
+    @IBOutlet weak var stylishPersona: UIImageView!
+    @IBOutlet weak var financePersona: UIImageView!
+    @IBOutlet weak var hipsterPersona: UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
