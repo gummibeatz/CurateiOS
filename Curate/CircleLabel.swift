55b1fbe9466bdeaf7ef2ebebbe856cbba0dce5d4
Linus Liang <linus.h.liang@gmail.com>
2015-12-12 22:09:38 -0500

    styled weightScaleView

---
+++ Curate/CircleLabel.swift
@@ -0,0 +1,38 @@
+//
+//  circleLabel.swift
+//  WheelControl
+//
+//  Created by Linus Liang on 12/12/15.
+//  Copyright © 2015 Linus Liang. All rights reserved.
+//
+
+import UIKit
+
+class CircleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func drawRect(rect: CGRect) {
+        let offset: CGFloat = 30
+        let borderWidth: CGFloat = 1
+        let centerPoint = CGPoint(x: rect.width/2, y: rect.height/2)
+        let outerPath = UIBezierPath(arcCenter: centerPoint, radius: rect.width / 2 - borderWidth, startAngle: 0, endAngle: 2 * π, clockwise: true)
+        let innerPath = UIBezierPath(arcCenter: centerPoint, radius: rect.width / 2 - borderWidth - offset, startAngle: 0, endAngle: 2 * π, clockwise: true)
+        curateBlue.setFill()
+        curateBlue.setStroke()
+        outerPath.stroke()
+        outerPath.fill()
+        UIColor.blackColor().setStroke()
+        curateDarkGrey.setFill()
+        innerPath.stroke()
+        innerPath.fill()
+        super.drawRect(rect)
+    }
+
+}
--- Curate/WeightScaleView.swift
+++ Curate/WeightScaleView.swift
@@ -11,11 +11,12 @@ import Foundation
 
 class WeightScaleView: UIView {
     
-    var containerView: SpinningWheelView?
+    var containerView: SpinningWheelView!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupContainerView()
+        setupLabel()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -25,11 +26,27 @@ class WeightScaleView: UIView {
     func setupContainerView() {
         let length = min(self.frame.height, self.frame.width)
         containerView = SpinningWheelView(frame: CGRect(x: (self.frame.width - length) / 2, y: (self.frame.height - length) / 2, width: length, height: length),  numberOfSections: 3)
-        containerView!.backgroundColor = UIColor.clearColor()
+        containerView.backgroundColor = UIColor.clearColor()
         let panningGesture = UIPanGestureRecognizer(target: self, action: "rotate:")
-        containerView?.addGestureRecognizer(panningGesture)
+        containerView.addGestureRecognizer(panningGesture)
         
         self.addSubview(containerView!)
+    }
+    
+    func setupLabel() {
+        let label = CircleLabel(frame: CGRect(x: 0, y: 0, width: containerView.bounds.width/2.5, height: containerView.bounds.height/2.5))
+        label.center = containerView.center
+        
+        let attributes = [NSFontAttributeName : UIFont(name: "Arial-BoldMT", size: 24)!, NSForegroundColorAttributeName : UIColor.whiteColor()]
+        let lbString = NSAttributedString(string: " lbs", attributes: attributes)
+        let text = NSMutableAttributedString(string: "20", attributes: attributes)
+        text.appendAttributedString(lbString)
+        
+        
+        label.attributedText = text
+        label.textAlignment = .Center
+        label.backgroundColor = UIColor.clearColor()
+        self.addSubview(label)
     }
     
     func rotate(sender: UIPanGestureRecognizer) {
@@ -37,8 +54,8 @@ class WeightScaleView: UIView {
         let location: CGPoint = sender.locationInView(self)
         let angle: CGFloat = getAngle(location: location, velocity: velocity)
         
-        let t = CGAffineTransformRotate(containerView!.transform, angle)
-        containerView?.transform = t
+        let t = CGAffineTransformRotate(containerView.transform, angle)
+        containerView.transform = t
         if abs(angle) > 0.1 {
             finishRotation(angle)
             
@@ -51,18 +68,18 @@ class WeightScaleView: UIView {
         let angle: CGFloat = sqrt(velocity.x * velocity.x + velocity.y * velocity.y)/4500
         print(location)
         //calculate kth vector of cross product to indicate direction
-        let containerCenter = self.convertPoint(containerView!.center, fromView: containerView)
+        let containerCenter = self.convertPoint(containerView.center, fromView: containerView)
         let x = location.x - containerCenter.x
         let y = location.y - containerCenter.y
-        print(containerView!.center)
+        print(containerView.center)
         print("x = \(x), y = \(y)")
         return (x * velocity.y - y * velocity.x > 0) ? angle : angle * -1
     }
     
     func finishRotation(xV: CGFloat) {
         UIView.animateWithDuration(0.7, delay: 0, options: [UIViewAnimationOptions.CurveEaseOut , UIViewAnimationOptions.AllowUserInteraction] , animations: {
-            let t = CGAffineTransformRotate(self.containerView!.transform, xV)
-            self.containerView!.transform = t
+            let t = CGAffineTransformRotate(self.containerView.transform, xV)
+            self.containerView.transform = t
             }, completion: nil)
     }
     
